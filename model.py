a09e105f5665865931e097d44760d8c8a02c5010
sanghamitraa <sangha.acharya5@gmail.com>
2022-01-04 11:45:46 +0530

    added 3 new models

--- model.py
+++ model.py
@@ -1,1 +1,11 @@
 print("all ml releted modeling here")
+def model1();
+   print("adding code for model1")
+
+
+   def model2();
+   print("adding code for model2")
+
+
+   def model3();
+   print("adding code for model3")
